a7064c62a9047320b6ab5988dc381aa211a8acad
Jérôme PAQUET <jerome@maestria.io>
2025-11-29 11:22:20 +0100

    chore: bump version to 1.0.2

--- src/cyvest/__init__.py
+++ src/cyvest/__init__.py
@@ -13,7 +13,7 @@ from cyvest.levels import Level
 from cyvest.model import CheckScorePolicy, ObservableType, RelationshipDirection, RelationshipType
 from cyvest.proxies import CheckProxy, ContainerProxy, EnrichmentProxy, ObservableProxy, ThreatIntelProxy
 
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 
 logger.disable("cyvest")
 

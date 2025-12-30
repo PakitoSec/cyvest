141f2fc4f43c15591470d00fb92dbf66b017f08e
PakitoSec <jeromep83@gmail.com>
2025-12-30 14:35:27 +0100

    chore: update version to 4.2.0 across all package files

--- src/cyvest/__init__.py
+++ src/cyvest/__init__.py
@@ -13,7 +13,7 @@ from cyvest.model import Check, Container, Enrichment, InvestigationWhitelist, O
 from cyvest.model_enums import ObservableType, RelationshipDirection, RelationshipType
 from cyvest.proxies import CheckProxy, ContainerProxy, EnrichmentProxy, ObservableProxy, ThreatIntelProxy
 
-__version__ = "4.1.0"
+__version__ = "4.2.0"
 
 logger.disable("cyvest")
 

df34111527567e4387701319bfe3146a59ec97ec
PakitoSec <jeromep83@gmail.com>
2026-01-07 16:00:35 +0100

    chore: update version to 5.0.1 across all package.json and related files

--- src/cyvest/__init__.py
+++ src/cyvest/__init__.py
@@ -13,7 +13,7 @@ from cyvest.model import Check, Enrichment, InvestigationWhitelist, Observable, 
 from cyvest.model_enums import ObservableType, RelationshipDirection, RelationshipType
 from cyvest.proxies import CheckProxy, EnrichmentProxy, ObservableProxy, TagProxy, ThreatIntelProxy
 
-__version__ = "5.0.0"
+__version__ = "5.0.1"
 
 logger.disable("cyvest")
 

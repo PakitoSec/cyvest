3548719bbcb33bc264a521de91e43e10b3b5fe01
PakitoSec <jeromep83@gmail.com>
2026-01-12 15:46:13 +0100

    chore: bump version to 5.1.1 across all packages

--- src/cyvest/__init__.py
+++ src/cyvest/__init__.py
@@ -21,7 +21,7 @@ from cyvest.model import Check, Enrichment, InvestigationWhitelist, Observable, 
 from cyvest.model_enums import ObservableType, RelationshipDirection, RelationshipType
 from cyvest.proxies import CheckProxy, EnrichmentProxy, ObservableProxy, TagProxy, ThreatIntelProxy
 
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 
 logger.disable("cyvest")
 

c70e190d10fe904503a612bac503afb9b32c8500
PakitoSec <jeromep83@gmail.com>
2026-01-19 12:51:59 +0100

    fix: correct import path for Ajv2020 to include .js extension

--- js/packages/cyvest-js/src/helpers.ts
+++ js/packages/cyvest-js/src/helpers.ts
@@ -1,4 +1,4 @@
-import Ajv2020, { type ValidateFunction } from "ajv/dist/2020";
+import Ajv2020, { type ValidateFunction } from "ajv/dist/2020.js";
 import addFormats from "ajv-formats";
 import schema from "../../../../schema/cyvest.schema.json" assert { type: "json" };
 import type { CyvestInvestigation } from "./types.generated";
